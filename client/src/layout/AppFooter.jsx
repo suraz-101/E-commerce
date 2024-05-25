9a86756e7734e6be682d7f88f411e853a6964310
Suraj Pandey <surazpandey101@gmail.com>
2024-05-25 23:12:16 +0545

    designing home page

--- client/src/layout/AppFooter.jsx
+++ client/src/layout/AppFooter.jsx
@@ -1,4 +1,17 @@
-
 export const AppFooter = () => {
-  return <div>AppFooter</div>;
+  return (
+    <div>
+      <footer className="bg-gray-200">
+        <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+          <a
+            href="#"
+            className="text-xl font-bold text-gray-500 hover:text-gray-400"
+          >
+            Brand
+          </a>
+          <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+        </div>
+      </footer>
+    </div>
+  );
 };
