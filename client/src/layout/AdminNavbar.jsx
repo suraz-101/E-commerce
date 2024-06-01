49e4a377992eade92e3caeb9a2c0496633bc2bf1
Suraj Pandey <surazpandey101@gmail.com>
2024-06-01 23:56:24 +0545

    admin logout handling

--- client/src/layout/AdminNavbar.jsx
+++ client/src/layout/AdminNavbar.jsx
@@ -1,3 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { removeToken } from "../utils/sessionManager";
+
 export const AdminNavbar = () => {
-  return <div>AdminNavbar</div>;
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    removeToken("token");
+    removeToken("currentUser");
+    navigate("/");
+  };
+  return (
+    <div className="border text-center p-2">
+      <button
+        className="border border-gray-200 px-4 py-1 bg-sky-500 text-white"
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
