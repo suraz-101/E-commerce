c7748fce86235f89bb52897187f0499eedcef639
Suraj Pandey <surazpandey101@gmail.com>
2024-06-12 22:16:38 +0545

    small modification

--- client/src/layout/AppNavbar.jsx
+++ client/src/layout/AppNavbar.jsx
@@ -72,7 +72,6 @@ export const AppNavbar = () => {
                   >
                     <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                   </svg>
-                  <div className="absolute top-10 left-0">{quantity}</div>
                 </button>
               </Link>
               <Link to="/checkOut">
