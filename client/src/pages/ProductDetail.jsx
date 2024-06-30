67cf9e8ffc34150679632b488bd30049fc8e9324
Suraj Pandey <surazpandey101@gmail.com>
2024-06-30 23:10:37 +0545

    trying to add rating stars in product detail page

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -8,6 +8,9 @@ import { Link } from "react-router-dom";
 import _ from "underscore";
 import { current } from "@reduxjs/toolkit";
 import { Comment } from "../components/Comment";
+import { FaStar } from "react-icons/fa";
+// var Rating = require("rating");
+// import { Rating } from "rating";
 
 export const ProductDetail = () => {
   const dispatch = useDispatch();
@@ -50,31 +53,65 @@ export const ProductDetail = () => {
                     Rs. {product[0]?.price}
                   </span>
                 </h1>
-                <p className="text-sm sm:text-base text-gray-700 dark:text-white">
-                  {product[0]?.description}
-                </p>
+                <div className="flex  ">
+                  {/* Stock : {product[0]?.stockQuantity} */}
+
+                  <div>
+                    {product[0]?.stockQuantity > 0 ? (
+                      <h1 className="text-green-500 mt-10 fw-bolder ">
+                        In Stock
+                      </h1>
+                    ) : (
+                      <h1 className="text-red-500 mt-10">Out of Stock</h1>
+                    )}
+                  </div>
+                </div>
+                <div className="flex">
+                  {[...Array(5)].map((star) => {
+                    return <FaStar className="text-orange-600" size={50} />;
+                  })}
+                </div>
+                {/* <p className="text-sm sm:text-base text-gray-700 dark:text-white">
+                  {product[0]?.description.slice(0, 100).concat("...")}
+                </p> */}
                 <div className="flex mt-8">
                   <a
                     href="#"
-                    className="uppercase py-2 px-4 rounded-lg bg-gradient-to-r from-cyan-400 to-sky-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400"
+                    className="uppercase py-2 px-4 rounded-lg bg-gradient-to-r from-cyan-400 to-sky-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400 flex "
                   >
-                    Get started
+                    <svg
+                      className="h-5 w-5 mr-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="black"
+                    >
+                      <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                    </svg>{" "}
+                    Add to Cart
                   </a>
-                  <a
-                    href="#"
+
+                  {/* <a
+                    onClick={() => {}}
                     className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-sky-500 text-pink-500 dark:text-white hover:bg-gradient-to-r from-cyan-400 to-sky-500  hover:text-white text-md"
                   >
                     Read more
-                  </a>
+                  </a> */}
                 </div>
               </div>
               <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
                 <img
                   src={BASE_URL.concat(product[0]?.image)}
                   className="max-w-xs h-full md:max-w-sm m-auto"
+                  height="400px"
+                  width="400px"
                 />
               </div>
             </div>
+          </div>
+          <div className="p-16">
+            <p className="text-sm sm:text-base text-gray-700 dark:text-white">
+              {product[0]?.description}
+            </p>
           </div>
         </main>
         <div className=" continer border mt-4 p-6 ">
